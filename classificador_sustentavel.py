2b8fa3ebdce0b3b9b9aed4ada3c1e23234acf48b
Rikk <tarikmoussa90@gmial.com>
2026-03-26 11:23:35 -0300

    finalização da entrada

--- classificador_sustentavel.py
+++ classificador_sustentavel.py
@@ -6,6 +6,12 @@
 #5:
 
 
+#Entrada de dados.
 
-print(" =============== Classificação de ações sustentáveis =============== ")
-impacto
+print("                                         =============== Classificação de ações sustentáveis =============== ")
+print()
+print()
+impacto = int(input("Como você avalia o impacto desta ação? ( Digite 1 - Baixo, 2 - Médio, 3 - Alto ): " ))
+vezes = int(input("Quantas vezes essa mesma ação foi realizada neste mês? ( Digite apenas um número. ): " ))
+validacao = input("Essa ação já foi validada pela plataforma, por foto, comprovante ou conferência? ( Sim/Não ): ")
+acao = input( "Essa ação foi coletiva ou gerou benefício para outras pessoas? ( Sim/Não ):  " )
