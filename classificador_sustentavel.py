f730bc6efac2460072d3e53bfd1222e2c2bb167b
Rikk <tarikmoussa90@gmial.com>
2026-03-26 11:10:25 -0300

    Implementação de membros e comeco do projeto

---
+++ classificador_sustentavel.py
@@ -0,0 +1,11 @@
+#Checkpoint de Computational Thinking Python; Integrantes:
+#1: Tárik Moussa Alma. - RM: 571411
+#2: Fábricio Aquiles - RM: 570985
+#3: Giovanne Dias - RM: 569750
+#4:
+#5:
+
+
+
+print(" =============== Classificação de ações sustentáveis =============== ")
+impacto
