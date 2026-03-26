6c6542cc17e7a8368b643c9e1032b7b4e9f9e873
Rikk <tarikmoussa90@gmial.com>
2026-03-26 11:47:49 -0300

    Classificador de prioridades.

--- classificador_sustentavel.py
+++ classificador_sustentavel.py
@@ -6,12 +6,28 @@
 #5:
 
 
-#Entrada de dados.
+
 
 print("                                         =============== Classificação de ações sustentáveis =============== ")
 print()
 print()
+#Entrada de dados.
 impacto = int(input("Como você avalia o impacto desta ação? ( Digite 1 - Baixo, 2 - Médio, 3 - Alto ): " ))
 vezes = int(input("Quantas vezes essa mesma ação foi realizada neste mês? ( Digite apenas um número. ): " ))
 validacao = input("Essa ação já foi validada pela plataforma, por foto, comprovante ou conferência? ( Sim/Não ): ")
 acao = input( "Essa ação foi coletiva ou gerou benefício para outras pessoas? ( Sim/Não ):  " )
+
+#Classificação de propriedades.
+if validacao == "Não":
+    classificacao = "Sem pontuação."
+elif impacto == 3 and acao == "Sim":
+    classificacao = "Destaque Sústentavel."
+elif impacto == 3 and vezes >= 5:
+    classificacao = "Pontuação Bônus."
+else:
+    classificacao = "Participação Registrada."
+
+#Mensagem de acordo com a prioridade.
+print("\n                                      ================================= Resultado =================================")
+if classificacao ==
+    acao =
