e2060a9ffad0be2a0971891c324909fe21d77631
giovanedc <163017837+Giovvane@users.noreply.github.com>
2026-03-27 10:17:33 -0300

    Arrumando o nome "Giovane"

--- classificador_sustentavel.py
+++ classificador_sustentavel.py
@@ -1,7 +1,7 @@
 #Checkpoint de Computational Thinking Python; Integrantes:
 #1: Tárik Moussa Alma. - RM: 571411
 #2: Fábricio Aquiles - RM: 570985
-#3: Giovanne Dias - RM: 569750
+#3: Giovane Dias - RM: 569750
 
 
 
